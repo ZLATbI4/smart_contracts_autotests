4be4e12ae0c88a3bd383e043f810ce1f5e2ac65d
Zlatych <y.zlatov@netpeak.net>
2022-10-23 11:29:17 +0300

    remove rnu_tests.sh file

--- run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm cache clean # clear all node caches
-npm install # install all stuff from package.json
-npx hardhat clean # clean hardhat compiled artifacts
-npx hardhat compile # compile smart-contracts
-npx hardhat coverage # run tests and show coverage
